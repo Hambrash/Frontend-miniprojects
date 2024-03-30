0df90db794d8c36971f782e1ef92c1a91d9f724c
Hambrash P B <137439574+Hambrash@users.noreply.github.com>
2024-03-31 01:05:49 +0530

    Add files via upload

---
+++ MiintyMarket/index.js
@@ -0,0 +1,11 @@
+var sidenav = document.querySelector('.side-navbar');
+
+function show() {
+    sidenav.style.display = "block"
+   
+}
+
+function close() {
+    sidenav.style.left = "-60%"
+   
+}
